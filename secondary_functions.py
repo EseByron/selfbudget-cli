2f03b2f194dd98ce9315311599fd402607b679cd
Byron Jaime Valencia <esebyron95@gmail.com>
2025-03-25 12:00:20 -0600

    Adding the first 2 essential files

---
+++ secondary_functions.py
@@ -0,0 +1,73 @@
+def get_name():
+  """Prompt the user to enter the name of a new budget and return it."""
+  name_input = input('Enter the name of your new budget: ')
+  return name_input.strip()
+
+def get_total_amount():
+  """Prompt the user to enter the total amount to divide and return it as a float."""
+  while True:
+    amount_input = input('Enter the total amount you want to divide: ')
+    try:
+      amount = float(amount_input)
+      return amount
+    except ValueError:
+      print('Invalid entry, use only numbers and fractions.')
+
+def slice_making():
+  """Prompt the user to enter slice names separated by commas and return a list of cleaned slice
+  names."""
+  raw_slices = input('Enter the name of the slices divided by a coma: ')
+  splitting_slices = raw_slices.split(',')
+  splitted_slices = []
+  for slice in splitting_slices:
+    splitted_slices.append(slice.strip())
+  return splitted_slices
+
+def percentage_definition(slices):
+  """Prompt the user to define percentages for each slice and return a list of percentages
+  as decimals for calculation."""
+  percentages = []
+  while True:
+    for slice in slices:
+      percentage = input(f'What\'s the percentage for {slice}: ')
+      try:
+        percentage = round(float(percentage), 2) * 0.01
+        percentages.append(percentage)
+      except ValueError:
+        print('Invalid input. Please enter a valid number.')
+        return percentage_definition(slices)
+    if sum(percentages) > 1:
+      print(f'Your percentages surpass 100%, try again. ({sum(percentages) * 100}%)')
+      percentage_definition(slices)
+    elif sum(percentages) < 1:
+      print(f'Your percentages do not get to a 100%, you\'re leaving {(1 - sum(percentages)) * 100}% behind.')
+      percentage_definition(slices)
+    return percentages
+
+def calculate_amounts(amount, slices, percentages):
+  """Calculate and return a dictionary mapping slices to their respective amounts based on
+  percentages."""
+  slices_with_amounts = {}
+  for slice, percentage in zip(slices, percentages):
+    slice_amount = round(amount * percentage, 2)
+    slices_with_amounts[slice] = slice_amount
+  return slices_with_amounts
+
+def print_budget(budget_data):
+  print('\n----------Budget information----------\n')
+  print(f'Budget name: {budget_data["Budget name"]}')
+  print(f'Total amount: ${budget_data["Total amount"]}')
+  print('\nSlices:')
+  for slice, amount in budget_data['Division'].items():
+    print(f'{slice}: ${amount:,.2f}')
+  print('\n--------------------------------------\n')
+
+def confirmation():
+  while True:
+    selection = input('Enter Y/y for yes or N/n for no: ')
+    if selection.lower() == 'y':
+      return True
+    elif selection.lower() == 'n':
+      return False
+    else:
+      print('Invalid input.')
---
+++ selfbudget_v2.0.py
@@ -0,0 +1,87 @@
+# MODULES
+
+import json
+import secondary_functions
+import os
+
+# MAIN FUNCTIONS
+
+def new_budget():
+  """
+  Creates a new budget by gathering user input for the budget name, total amount, 
+  categories, and their percentages. Calculates amounts for each category and 
+  optionally saves the data to a JSON file.
+
+  Returns:
+    None
+  """
+  budget_name = secondary_functions.get_name()
+  amount = secondary_functions.get_total_amount()
+  slices = secondary_functions.slice_making()
+  percentages = secondary_functions.percentage_definition(slices)
+  slices_with_amounts = secondary_functions.calculate_amounts(amount, slices, percentages)
+  budget_data = {
+    'Budget name': budget_name,
+    'Total amount': amount,
+    'Division': slices_with_amounts,
+  }
+
+  """Printing the last made budget info."""
+  secondary_functions.print_budget(budget_data)
+
+  print('Would you like to save a .json file with this information?')
+  if secondary_functions.confirmation():
+    file_name = input('Name your file: ')
+    with open(f'{file_name}.json', 'w') as budget_file:
+      json.dump(budget_data, budget_file, indent=4)
+  else:
+    return budget_data
+
+def load_budget():
+  """
+  Prompts the user to load a budget file in JSON format, handles file not found errors, 
+  and displays the loaded budget details.
+
+  Returns:
+    dict: The loaded budget data if the file is successfully found and parsed.
+  """
+  budget_selection = input('Enter the name of the budget file you want to load: ')
+  try:
+    with open(f'{budget_selection}.json') as budget_file:
+      loaded_budget = json.load(budget_file)
+  except FileNotFoundError:
+    print(f'The file {budget_selection}.json was not found, try again.')
+    load_budget()
+  else:
+    secondary_functions.print_budget(loaded_budget)
+    return loaded_budget
+
+def delete_budget():
+  budget_selection = input('Enter the name of the budget file your want to delete: ')
+  if os.path.exists(f'{budget_selection}.json'):
+    secondary_functions.confirmation()
+
+
+# WORKFLOW
+
+welcome_message = '\n Hello, welcome to the budget calculator by EseByron.\n'
+print(welcome_message)
+
+while True:
+  main_menu = 'MAIN MENU:\n' \
+  '1) New budget\n' \
+  '2) Load budget\n' \
+  '3) Delete budget\n' \
+  '4) Edit budget\n' \
+  '0) Exit program'
+  print(main_menu)
+  
+  selection = input('Select an option: ')
+  if selection == '1':
+    loaded_budget = new_budget()
+  elif selection == '2':
+    loaded_budget = load_budget()
+  elif selection == '3':
+    delete_budget()
+  elif selection == '0':
+    break
